8995f782a081d4a990f689b2c8e53cf238cf6f88
Nathan Monette <nathanmonette1@gmail.com>
2024-04-21 09:58:38 -0700

    Added visusalizer, fixed type issues

--- ipg_jax/environments/gridworld.py
+++ ipg_jax/environments/gridworld.py
@@ -25,9 +25,9 @@ class AdvMultiGrid(environment.Environment):
     def __init__(self, dim, max_time, partial = True):
         super().__init__()
         self.dim = dim
-        self.walls = jnp.pad(jnp.full((dim - 1, dim - 1), False), ((1, 1), (1, 1)), constant_values=True)
+        self.walls = jnp.pad(jnp.full((dim - 1, dim - 1), False, jnp.int32), ((1, 1), (1, 1)), constant_values=True)
         self.max_time = max_time
-        self.possible_coords = jnp.array([(i,j) for i in range(dim) for j in range(dim)])
+        self.possible_coords = jnp.array([(i,j) for i in range(dim) for j in range(dim)], dtype=jnp.int32)
 
         self.num_agents = 3
         self.partial = partial
@@ -38,32 +38,32 @@ class AdvMultiGrid(environment.Environment):
         goal1 = self.possible_coords[goals[0]]
         goal2 = self.possible_coords[goals[1]]
 
-        probs = 1 / (self.dim * self.dim - 2) * jnp.array(jnp.logical_and(jnp.arange(self.dim * self.dim) != goals[0], jnp.arange(self.dim * self.dim) != goals[1]), dtype=float)
+        probs = 1 / (self.dim * self.dim - 2) * jnp.array(jnp.logical_and(jnp.arange(self.dim * self.dim) != goals[0], jnp.arange(self.dim * self.dim) != goals[1]), dtype=jnp.float32)
 
         agents = jax.random.choice(agent_rng, self.possible_coords, shape=(3,), p=probs)
         return EnvState(
-            jax.vmap(lambda i,p: Obj(i, p, jnp.array(1), jnp.array(i == 2)), in_axes=(0, 0))(jnp.arange(3), agents),
-            Obj(0, goal1, jnp.array(1)),
-            Obj(1, goal2, jnp.array(1)),
-            jnp.array(0),
-            jnp.array(0)
+            jax.vmap(lambda i,p: Obj(i, p, jnp.array(1,jnp.int32), jnp.array(i == 2, jnp.int32)), in_axes=(0, 0))(jnp.arange(3), agents),
+            Obj(0, goal1, jnp.array(1, jnp.int32)),
+            Obj(1, goal2, jnp.array(1, jnp.int32)),
+            jnp.array(0, jnp.int32),
+            jnp.array(0, jnp.int32)
         )
 
     def _handle_actions(self, pos, action):
-        step = (action == 0) * jnp.array([0, -1]) \
-             + (action == 1) * jnp.array([0, 1]) \
-             + (action == 2) * jnp.array([1, 0]) \
-             + (action == 3) * jnp.array([-1, 0])
+        step = jnp.int32(action == 0) * jnp.array([0, -1], jnp.int32) \
+             + jnp.int32(action == 1) * jnp.array([0, 1], jnp.int32) \
+             + jnp.int32(action == 2) * jnp.array([1, 0], jnp.int32) \
+             + jnp.int32(action == 3) * jnp.array([-1, 0], jnp.int32)
         
         pos = pos + step
 
         return jnp.clip(pos, 0, self.dim - 1)
     
     def _handle_rewards(self, state, agent):
-        val1 = jnp.array_equal(agent.pos, state.goal1.pos) * state.goal1.active
-        val2 = jnp.array_equal(agent.pos, state.goal2.pos) * state.goal2.active
+        val1 =jnp.int32(jnp.array_equal(agent.pos, state.goal1.pos)) * state.goal1.active
+        val2 = jnp.int32(jnp.array_equal(agent.pos, state.goal2.pos)) * state.goal2.active
         val = val1 + val2
-        reward = ((1 - agent.adv) * jnp.full((self.num_agents, ), val).at[-1].set(-val) + agent.adv * jnp.full((self.num_agents, ), -val).at[-1].set(val))
+        reward = ((1 - agent.adv) * jnp.full((self.num_agents, ), val, jnp.int32).at[-1].set(-val) + agent.adv * jnp.full((self.num_agents, ), -val, jnp.int32).at[-1].set(val))
         return reward, val1, val2
 
     def get_obs(self, state):
@@ -73,22 +73,22 @@ class AdvMultiGrid(environment.Environment):
         
         def partial_extract_agent_obs():
             goals = jnp.concatenate((state.goal1.pos, state.goal1.active.reshape(1 ), state.goal2.pos, state.goal2.active.reshape(1)))
-            goals = jnp.repeat(goals.reshape((-1, 6)), jnp.array((3, )), axis=0, total_repeat_length=3)
+            goals = jnp.repeat(goals.reshape((-1, 6)), jnp.array((3, ), jnp.int32), axis=0, total_repeat_length=3)
             return jnp.append(state.agent.pos, goals, 1)
         
         if self.partial:
             return jnp.int32(partial_extract_agent_obs())
         else:
-            return jnp.int32(jnp.repeat(extract_agent_obs().reshape(-1, 15), jnp.array((3, )), axis=0, total_repeat_length=3))
+            return jnp.int32(jnp.repeat(extract_agent_obs().reshape(-1, 15), jnp.array((3, ), jnp.int32), axis=0, total_repeat_length=3))
     
     def step_env(self, _, state: EnvState, action: jnp.ndarray, *args, **kwargs):
         pos = jax.vmap(self._handle_actions)(state.agent.pos, action)
         reward, term1, term2 = jax.vmap(self._handle_rewards, in_axes=(None, 0))(state, state.agent)
 
-        agent = state.agent.replace(pos=pos, active=jnp.logical_not(term1 + term2))
-        state = state.replace(time = state.time + 1, agent=agent, goal1=state.goal1.replace(active=jnp.logical_not(jnp.sum(term1))), goal2=state.goal2.replace(active=jnp.logical_not(jnp.sum(term2))))
+        agent = state.agent.replace(pos=pos, active=jnp.int32(jnp.logical_not(term1 + term2)))
+        state = state.replace(time = state.time + 1, agent=agent, goal1=state.goal1.replace(active=jnp.int32(jnp.logical_not(jnp.sum(term1)))), goal2=state.goal2.replace(active=jnp.int32(jnp.logical_not(jnp.sum(term2)))))
         
-        done = jnp.logical_or(state.time == self.max_time, jnp.logical_or(jnp.sum(state.agent.active[:-1]) == 0, state.agent.active[-1] == 0), )
+        done = jnp.int32(jnp.logical_or(state.time == self.max_time, jnp.logical_or(jnp.sum(state.agent.active[:-1]) == 0, jnp.int32(state.agent.active[-1]) == 0), ))
         return (
             jax.lax.stop_gradient(self.get_obs(state)),
             jax.lax.stop_gradient(state),
--- ipg_jax/train.py
+++ ipg_jax/train.py
@@ -1,11 +1,11 @@
 import jax
 import jax.numpy as jnp
 
-from environments import RolloutWrapper
+from environments import RolloutWrapper, GridVisualizer
 from agents.policy import DirectPolicy
 from utils import parse_args, compute_nash_gap
 
-import sys
+import sys, os
 
 def make_train(args):
     def ipg_train_fn(rng):
@@ -45,7 +45,7 @@ def make_train(args):
                         idx_fn = lambda idx: lambda_[tuple(idx)]
                         lambdas = jax.vmap(idx_fn)(idx)
 
-                        loss = jax.vmap(fn)(data.reward[:, -1], lambdas, idx)
+                        loss = jax.vmap(fn)(jnp.float32(data.reward[:, -1]), lambdas, idx)
 
                         disc = jnp.cumprod(jnp.ones_like(loss) * args.gamma) / args.gamma
                         return jnp.dot(loss, disc)
@@ -87,7 +87,7 @@ def make_train(args):
 
                     return jnp.dot(log_probs, returns)
                 
-                return jax.vmap(episode_loss, in_axes=(None, 0, 0, 0))(params, data.reward[:, idx], data.obs[:, idx], data.action[:, idx]).mean()
+                return jax.vmap(episode_loss, in_axes=(None, 0, 0, 0))(params, jnp.float32(data.reward[:, idx]), data.obs[:, idx], data.action[:, idx]).mean()
 
             grads = jax.vmap(outer_loss, in_axes=(0, None, 0))(agent_params[:agent_params.shape[0] - 1], data, jnp.arange(agent_params.shape[0] - 1))
 
@@ -100,11 +100,17 @@ def make_train(args):
             
             rng, _rng = jax.random.split(rng)
 
-            return (rng, agent_params), compute_nash_gap(_rng, args, policy, agent_params, rollout)
+            return (rng, agent_params), None # compute_nash_gap(_rng, args, policy, agent_params, rollout)
 
-        carry_out, nash_gap = jax.lax.scan(train_loop, (rng, agent_params), jnp.arange(args.iters), 1000)
+        carry_out, nash_gap = jax.lax.scan(train_loop, (rng, agent_params), jnp.arange(args.iters), args.iters)
 
-        return nash_gap
+        rng, agent_params = carry_out
+
+        rng, reset_rng, rollout_rng = jax.random.split(rng, 3)
+        init_obs, init_state = rollout.env.reset(reset_rng)
+        states = rollout.render_rollout(rollout_rng, agent_params, init_obs, init_state, False)
+
+        return nash_gap, agent_params, states
 
     return ipg_train_fn
 
@@ -112,10 +118,22 @@ def main(cmd=sys.argv[1:]):
     args = parse_args(cmd)
     rng = jax.random.key(args.seed)
     train_fn = make_train(args)
-    # with jax.numpy_dtype_promotion('strict'):
-    nash_gap = jax.jit(train_fn)(rng)
+    with jax.numpy_dtype_promotion('strict'):
+        nash_gap, agent_params, states = jax.jit(train_fn)(rng)
+
+
+    os.makedirs("output", exist_ok=True)
+    experiment_num = len(list(os.walk('./output')))
+    os.makedirs(f"output/experiment-{experiment_num}")
+
+    with jax.disable_jit(True):
+        gv = GridVisualizer({"dim": args.dim, "max_time":12}, states, None)
+        gv.animate(view=True)
+
+    for agent in range(len(agent_params)):
+        jnp.save(f"output/experiment-{experiment_num}/agent{agent+1}", agent_params[agent])
     
-    print(jnp.max(nash_gap, 1))
+    # print(jnp.max(nash_gap, 1))
 
 if __name__ == "__main__":
     main()
